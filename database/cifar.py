3d83169d595b30b14ba1e958bd7e822fea11c2bd
Kohaku-Blueleaf <KohakuBlueleaf@users.noreply.github.com>
2020-01-18 11:55:20 +0800

    database

---
+++ database/cifar.py
@@ -0,0 +1,53 @@
+# coding: utf-8
+import pickle
+from random import randint as rand
+from functions import _change_one_hot_label,label_smoothing
+import numpy as np
+from PIL import Image
+
+
+file = './cifar/data_batch_'
+
+def load_cifar(normalize=True, flatten=True, one_hot_label=False, smooth=False):
+	with open(file+str(1), 'rb',) as f:
+		dataset1 = pickle.load(f,encoding='bytes')
+	with open(file+str(2), 'rb',) as f:
+		dataset2 = pickle.load(f,encoding='bytes')
+	with open(file+str(3), 'rb',) as f:
+		dataset3 = pickle.load(f,encoding='bytes')
+	with open(file+str(4), 'rb',) as f:
+		dataset4 = pickle.load(f,encoding='bytes')
+	with open(file+str(5), 'rb',) as f:
+		dataset5 = pickle.load(f,encoding='bytes')
+	dataset = {}
+	with open('./cifar/test_batch', 'rb') as f:
+		testset = pickle.load(f,encoding='bytes')
+	
+	dataset[b'data'] = np.vstack((dataset1[b'data'],dataset2[b'data'],dataset3[b'data'],dataset4[b'data'],dataset5[b'data']))
+	dataset[b'labels'] = np.hstack((dataset1[b'labels'],dataset2[b'labels'],dataset3[b'labels'],dataset4[b'labels'],dataset5[b'labels']))
+	
+	
+	if normalize:
+		dataset[b'data'] = dataset[b'data'].astype(np.float32)
+		dataset[b'data'] /= 255.0
+		testset[b'data'] = testset[b'data'].astype(np.float32)
+		testset[b'data'] /= 255.0
+
+	if one_hot_label:
+		dataset[b'labels'] = _change_one_hot_label(dataset[b'labels'],10)
+		testset[b'labels'] = _change_one_hot_label(testset[b'labels'],10)
+
+	if not flatten:
+		dataset[b'data'] = dataset[b'data'].reshape(50000, 3, 32, 32)
+		testset[b'data'] = testset[b'data'].reshape(10000, 3, 32, 32)
+	
+	if smooth:
+		dataset[b'labels'] = label_smoothing(dataset[b'labels'],0.1)
+		testset[b'labels'] = label_smoothing(testset[b'labels'],0.1)
+	
+	return (dataset[b'data'], dataset[b'labels']), (testset[b'data'], testset[b'labels'])
+
+if __name__ == '__main__':
+	(a,b),(c,d) = load_cifar(False,False)
+	img=Image.fromarray(a[0].transpose(1,2,0),'RGB')
+	img.show()
---
+++ database/emnist.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+import pickle
+from random import randint as rand
+import numpy as np
+import gzip
+from functions import _change_one_hot_label, label_smoothing
+
+img_size = 784
+dataset = {}
+testset = {}
+file = {
+	'train_img':'./gzip/emnist-letters-train-images-idx3-ubyte.gz',
+	'train_label':'./gzip/emnist-letters-train-labels-idx1-ubyte.gz',
+	'test_img':'./gzip/emnist-letters-test-images-idx3-ubyte.gz',
+	'test_label':'./gzip/emnist-letters-test-labels-idx1-ubyte.gz'
+}
+
+def load_labels(file):
+	with gzip.open(file, 'r',) as f:
+		labels = np.frombuffer(f.read(), np.uint8, offset=8)
+	
+	return labels
+
+def load_imgs(file):
+	with gzip.open(file, 'r',) as f:
+		imgs = np.frombuffer(f.read(), np.uint8, offset=16)
+	imgs = imgs.reshape(-1, img_size)
+	return imgs
+
+def load_emnist(normalize=True, flatten=True, one_hot_label=True, smooth=False, choose = 0):
+	dataset['data'] = load_imgs(file['train_img'])
+	testset['data'] = load_imgs(file['test_img'])
+	dataset['labels'] = load_labels(file['train_label'])
+	testset['labels'] = load_labels(file['test_label'])
+
+	if normalize:
+		dataset['data'] = dataset['data'].astype(np.float32)
+		dataset['data'] /= 255.0
+		testset['data'] = testset['data'].astype(np.float32)
+		testset['data'] /= 255.0
+
+	if one_hot_label:
+		dataset['labels'] = _change_one_hot_label(dataset['labels'],26)
+		testset['labels'] = _change_one_hot_label(testset['labels'],26)
+
+	if not flatten:
+		dataset['data'] = dataset['data'].reshape(-1, 1, 28, 28)
+		testset['data'] = testset['data'].reshape(-1, 1, 28, 28)
+	
+	if choose == 0:
+		data_choose = np.arange(0,len(dataset['data']))
+		test_choose = np.arange(0,len(testset['data']))
+	elif choose == 1:
+		data_choose = np.arange(1,len(dataset['data'])+1,2)
+		test_choose = np.arange(1,len(testset['data'])+1,2)
+	elif choose == 2:
+		data_choose = np.arange(0,len(dataset['data']),2)
+		test_choose = np.arange(0,len(testset['data']),2)
+	
+	if smooth:
+		dataset['labels'] = label_smoothing(dataset['labels'],0.1)
+		testset['labels'] = label_smoothing(testset['labels'],0.1)
+	
+	return (dataset['data'][data_choose], dataset['labels'][data_choose]), (testset['data'][test_choose], testset['labels'][test_choose])
+
+
+if __name__ == '__main__':
+	(x_train, t_train), (x_test, t_test) = load_emnist(flatten=False, one_hot_label=False)
+	print(x_train.shape)
+	print(t_train.shape)
+	print(x_test.shape)
+	print(t_test.shape)
