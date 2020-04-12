7671ce0efab1558328ffe58d34987b37e58a8f0d
Kohaku-Blueleaf <KohakuBlueleaf@users.noreply.github.com>
2020-04-12 22:12:44 +0800

    temp

--- BlueNet/setting.py
+++
@@ -1,8 +0,0 @@
-try:
-	import cupy as _np
-	import cupyx.scipy.special.erf as _erf
-	from cupy import exp as _exp
-except:
-	import numpy as _np
-	from scipy.special import _erf
-	from numpy import _exp
