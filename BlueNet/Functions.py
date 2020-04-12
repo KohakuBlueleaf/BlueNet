81f2449f9d0f330e6be7c09909712403df52f696
Kohaku-Blueleaf <KohakuBlueleaf@users.noreply.github.com>
2020-04-12 22:23:17 +0800

    Debug

--- BlueNet/Functions.py
+++ BlueNet/Functions.py
@@ -11,55 +11,42 @@ Activation Functions
 sqrt2 = 2**0.5
 
 def identity_function(x):
-	
 	return x
 
 def step_function(x): #Binary
-	
 	return _np.array(x > 0, dtype=_np.int)
 
 def sigmoid(x):
-	
 	return 1 / (1 + _np.exp(-x))	
 	
 def sigmoid_grad(x):
-	
 	return (1.0 - sigmoid(x)) * sigmoid(x)
 
 def softplus(x):
-	
 	return _np.log(1+_np.exp(x))
 
 def softplus_grad(x):
-	
 	return sigmoid(x)
 
 def elu(x):
-	
 	return _np.exp(x)-1
 	
 def elu_grad(x):
-
 	return _np.exp(_np.exp(x)-1)+1
 
 def sinh(x):
-	
 	return ((_np.exp(x) - _np.exp(-x)) / 2)
 
 def cosh(x):
-	
 	return ((_np.exp(x) + _np.exp(-x)) / 2)
 
 def tanh(x):
-	
 	return sinh(x) / cosh(x)
 
 def tanh_grad(x):
-	
 	return 1-(tanh(x)**2)
 
 def relu(x):
-	
 	return _np.maximum(0, x)
 
 def relu_grad(x):
@@ -69,51 +56,39 @@ def relu_grad(x):
 	return grad
 	
 def arctan(x):
-
 	return(_np.arctan(x))
 	
 def arctan_grad(x):
-
 	return(1/(x**2+1))
 
 def softsign(x):
-	
 	return x/(_np.abs(x)+1)
 	
 def softsign_grad(x):
-	
 	return 1/(_np.abs(x)+1)**2
 
 def softclip(x):
-	
 	return(_np.log((1+_np.exp(x))/(1+_np.exp(x-1))))
 	
 def softclip_grad(x):
-
 	return((-1+_np.e)*_np.exp(x))/((_np.exp(x)+1)*(_np.exp(x)+_np.e))
 
 def isru(x,a=1):
-
 	return x*(1/(1+a*x**2)**0.5)
 
 def isru_grad(x,a=1):
-
 	return (1/(1+a*x**2)**0.5)**3
 	
 def isru_bound(a):
-	
 	return (1/a**0.5),(-1/a**0.5)
 
 def erf_grad(x):
-	
 	return (2/_np.pi**0.5)*(_np.exp(-x**2))
 
 def gelu_erf(x): #use scipy.special._erf/cupyx.scipy.special._erf
-	
 	return 0.5*x*(1+_erf(x/sqrt2))
 
 def gelu_erf_grad(x):
-	
 	return 0.25*(sqrt2*x*_erf_grad(x/sqrt2)+2*_erf(x/sqrt2)+2)
 
 
@@ -121,19 +96,19 @@ def gelu_erf_grad(x):
 Other Functions
 '''
 
-# 将整数表示成为binary_dim位的二进制数，高位用0补齐
 def int_2_binary(number, binary_dim):
     binary_list = list(map(lambda x: int(x), bin(number)[2:]))
     number_dim = len(binary_list)
     result_list = [0]*(binary_dim-number_dim)+binary_list
-    return result_list
+    
+	return result_list
 
-# 将一个二进制数组转为整数
 def binary2int(binary_array):
     out = 0
     for index, x in enumerate(reversed(binary_array)):
         out += x * pow(2, index)
-    return out
+    
+	return out
 
 def get_binary_data(BINARY_DIM):
 	binary = numpy.array([int_2_binary(x, BINARY_DIM) for x in range(2**BINARY_DIM)])
@@ -149,7 +124,6 @@ def get_binary_data(BINARY_DIM):
 	return (numpy.reshape(dataX, (len(dataX), BINARY_DIM*2,1)),numpy.array(dataY))
 
 def mean_squared_error(y, t):
-	
 	return 0.5 * _np.sum((y-t+1e-6)**2)
 
 def cross_entropy_error(y, t):
@@ -208,7 +182,7 @@ def numerical_gradient_1d(f, x):
 		fxh2 = f(x) # f(x-h)
 		grad[idx] = (fxh1 - fxh2) / (2*h)
 		
-		x[idx] = tmp_val # 値を元に戻す
+		x[idx] = tmp_val
 		
 	return grad
 
@@ -238,7 +212,7 @@ def numerical_gradient(f, x):
 		fxh2 = f(x) # f(x-h)
 		grad[idx] = (fxh1 - fxh2) / (2*h)
 		
-		x[idx] = tmp_val # 値を元に戻す
+		x[idx] = tmp_val
 		it.iternext()   
 		
 	return grad
@@ -277,7 +251,6 @@ def shuffle_dataset(x, t):
 	return x, t
 
 def conv_output_size(input_size, filter_size, stride=1, pad=0):
-	
 	return (input_size + 2*pad - filter_size) / stride + 1
 
 def im2col(input_data, filter_h, filter_w, stride=1, pad=0, type=_np.float32):
@@ -362,7 +335,6 @@ def cos_similarity(x,y):
 	return _np.dot(nx,ny)
 
 def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
-	
 	if query not in word_to_id:
 		print(query,'is not found')
 		return
@@ -407,5 +379,3 @@ def ppmi(C, verbose=False, eps=1e-8):
 	print('100.0% done')
 	
 	return M
-	
-
